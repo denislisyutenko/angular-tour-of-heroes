f17eda36da4f6e31b02d2155f77cd90dda0425cf
Denis Lisyutenko <denislisyutenko@gmail.com>
2023-03-26 00:07:08 +0200

    Delete hero

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -68,4 +68,14 @@ import { catchError, tap } from 'rxjs/operators';
     );
   }
 
+  /** DELETE: delete the hero from the server */
+  deleteHero(id: number): Observable<Hero> {
+    const url = `${this.heroesUrl}/${id}`;
+
+    return this.http.delete<Hero>(url, this.httpOptions).pipe(
+      tap(_ => this.log(`deleted hero id=${id}`)),
+      catchError(this.handleError<Hero>('deleteHero'))
+    );
+  }
+
 }
